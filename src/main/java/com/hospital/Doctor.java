12e6b14da10348ad39b933b909c4cd9e90ee00d5
newlin-grace1 <newlingrace1@gmail.com>
2024-05-31 10:25:52 +0530

    Hospital management using JDBC

---
+++ src/main/java/com/hospital/Doctor.java
@@ -0,0 +1,47 @@
+package com.hospital;
+import java.sql.*;
+public class Doctor {
+	private Connection connection;
+
+    public Doctor(Connection connection){
+        this.connection = connection;
+    }
+
+    public void viewDoctors(){
+        String query = "select * from doctors";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            System.out.println("Doctors ");
+            System.out.println("| Doctor Id  | Name               | Specialization   |");
+            System.out.println("--------------------------------------------------");
+            while(resultSet.next()){
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String specialization = resultSet.getString("specialization");
+                System.out.printf("| %s | %s | %s |\n", id, name, specialization);
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public boolean getDoctorById(int id){
+        String query = "SELECT * FROM doctors WHERE id = ?";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return true;
+            }else{
+                return false;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
+
